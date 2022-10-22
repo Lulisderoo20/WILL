8be88f06d3cfe27ff71d66f3cc006cd8fbda011e
Lulisderoo20 <92049842+Lulisderoo20@users.noreply.github.com>
2022-10-22 04:47:04 -0300

    Update 02.js

--- 02.js
+++ 02.js
@@ -12,6 +12,12 @@ function stringMasLarga(strings) {
   // stringMasLarga(['JavaScript', 'HTML', 'CSS']); debe retornar 'JavaScript'
 
   // Tu código aca
+   let largo='';
+for (var i = 0; i < strings.length; i++) {
+  if(largo.length<strings[i].length){
+    largo= strings[i];
+  }
+}return largo;
 }
 
 // No modifiques nada debajo de esta linea //
